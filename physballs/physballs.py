2c734627cd007882238466b045a78f4c2b6d780c
Andres Martinez <mewsiof@gmail.com>
2020-07-07 07:00:02 -0600

    Reorganize files
    -Add new root folder Physballs.iml

--- physballs/physballs.py
+++ physballs/physballs.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-import time
-import pygame
-import sys
-
-from render import open_window
-
 # TODO instead of creating objects within your game loop,
 # create them once somewhere else and store
 # them in an array somewhere. then, inside
