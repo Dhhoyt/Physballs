83e11e55cfe39af4c46022c41824dc21b6c517b0
Andres Martinez <mewsiof@gmail.com>
2020-07-08 05:05:27 -0600

    Implement planet creation in game loop using lists.

--- physballs/planets.py
+++ physballs/planets.py
@@ -1,5 +1,5 @@
 import pygame
-from render import screen
+import render
 
 planets = []
 
@@ -12,11 +12,13 @@ class Planet:
         self.y = y
         self.size = size
         self.color = (255, 160, 153)
-        self.thickness = 40
+        self.thickness = size
 
     def display(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size, self.thickness)
+        pygame.draw.circle(render.screen, self.color, (self.x, self.y), self.size, self.thickness)
 
 
-def create_planet(x_pos, y_pos, a_size):
+def create_planet(x_pos, y_pos, a_size) -> list:
     planets.append(Planet(x_pos, y_pos, a_size))
+    return planets
+
--- physballs/render.py
+++ physballs/render.py
@@ -2,6 +2,7 @@ import pygame
 import time
 import sys
 import controls
+import planets
 
 (width, height) = (1200, 720)
 screen = pygame.display.set_mode((width, height), 0, 32)
@@ -21,6 +22,7 @@ def open_window():
 
 def loop(a_screen, bg_color, running):
     last_time = time.time()
+
     # Game loop starts here.
     while running:
 
@@ -33,21 +35,23 @@ def loop(a_screen, bg_color, running):
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
-                running = False  # Be IDLE friendly
+                running = False
                 sys.exit()
 
-        a_screen.fill(bg_color)
+        for planet in planets.planets:
+            a_screen.fill(bg_color)
+            display_text(str(planets.create_planet(midScreenX, midScreenY, 40)), a_screen, bg_color, (80, 80))
+
+        # a_screen.fill(bg_color)
+        display_text("Pointer coordinates: " + str(controls.get_mouse_pos()), a_screen, bg_color, (10, 10))
         # Make the most recently drawn screen visible.
-        display_text("Pointer coordinates: " + str(controls.get_mouse_pos()), a_screen, bg_color)
         pygame.display.flip()
 
     pygame.quit()
 
 
-# TODO in the future use pygame.font.get_init() to debug.
-
-def display_text(text, a_screen, bg_color):
+def display_text(text, a_screen, bg_color, pos: tuple):
     a_screen.fill(bg_color)
     text_font = pygame.font.SysFont("consolas", 14)
     text_surface = text_font.render(text, False, (0, 255, 0))
-    screen.blit(text_surface, (10, 10))
+    screen.blit(text_surface, pos)
